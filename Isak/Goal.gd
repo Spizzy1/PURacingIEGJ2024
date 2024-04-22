d779b415260fa0b4b7e5c478f55fd79feb56ebf8
Spizzy1 <isak.attila@gmail.com>
2024-04-22 16:08:01 +0200

    This is such a real build

---
+++ Isak/Goal.gd
@@ -0,0 +1,17 @@
+extends Area3D
+
+signal player_entered
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body.name == "Vehicle Body2":
+		player_entered.emit()
+	pass # Replace with function body.
