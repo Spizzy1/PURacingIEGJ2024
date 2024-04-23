ba3bce1d29cde47f0789ded804c5a54b793d467c
Blixt <blixtdraken@gmail.com>
2024-04-23 11:04:45 +0200

    Fixed more car movement

--- Pond/CarMovement.gd
+++ Pond/CarMovement.gd
@@ -6,6 +6,12 @@ var wheels: Array = [];
 var engine_max: float = 150
 @export
 var steering_max_deg: float = 10
+@export
+var brake_force: float = 50
+
+
+@export
+var angular_correction_amount: float = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(0,4):
@@ -20,8 +26,13 @@ func _process(delta):
 	var wasd = Input.get_vector("Left", "Right","Backwards","Forwards")
 	engine_force = wasd.y*engine_max
 	steering = deg_to_rad(wasd.x*steering_max_deg* -1)
-	
-	
-	
+	if Input.is_action_pressed("Break"):
+		brake = brake_force
+	else:
+		brake = 0
+	print(str(rotation.x) + " and " + str(rotation.z))
+	#rotation.x = (rotation.x * angular_correction_amount)
+	#rotation.z = (rotation.z * angular_correction_amount)
+	print(str(rotation.x) + " and " + str(rotation.z))
 	
 	pass
