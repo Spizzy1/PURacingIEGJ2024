7acfd3ab6a3d7db758d457a459bf3368675007f4
Blixt <53824895+Blixtdraken@users.noreply.github.com>
2024-04-24 17:37:52 +0200

    Final fixes

--- SettingsGlobal.gd
+++ SettingsGlobal.gd
@@ -1,8 +1,8 @@
 extends Node
 
-var default_ADRESS = "127.0.0.1"
+var default_ADRESS = "185.234.69.250"
 var default_PORT = 4576
-var ADRESS = "127.0.0.1"
+var ADRESS = "185.234.69.250"
 var PORT = 4576
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- SettingsMenu.gd
+++ SettingsMenu.gd
@@ -1,5 +1,7 @@
 extends MarginContainer
 
+var singleplayer_scene = preload("res://MainMenuScene.tscn")
+var networking_scene = preload("res://Networking-Settings.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +11,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_networking_pressed():
+	var instance = networking_scene.instantiate()
+	get_node("HBoxContainer").add_child(instance)
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	var instance = singleplayer_scene.instantiate()
+	get_parent().add_child(instance)
+	queue_free()
+	pass # Replace with function body.
