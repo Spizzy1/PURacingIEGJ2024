843a709cc13fbc1390d24b85b67c2d75d1d3c0fa
baptul-bim <baptul.bim@gmail.com>
2024-04-25 13:18:41 +0200

    victory manager

--- Louie/VictoryManager.gd
+++ Louie/VictoryManager.gd
@@ -2,12 +2,36 @@ extends Node
 
 
 var _victory = false
+var victory_timer
+var victory_scene = preload("res://Louie/VictoryScene.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	victory_timer = get_node("Timer")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+
+	if _victory:
+		Engine.time_scale = Engine.time_scale*0.01
+
+		##if Engine.time_scale<0.01:
+		_victory_scene_changer()
+		##pass
+
 	pass
+
+
+func _victory_scene_changer():
+	
+	_victory=false
+	victory_timer.start()
+	
+	pass
+
+func _on_timer_timeout():
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
+	pass # Replace with function body.
